188cabff0e89d2110e2894303a55cfe22aade44c
Your Name <huffman.hayden@gmail.com>
2024-09-25 12:01:32 -0400

    Add global styling. Create components and layouts. Refactor all pages to use component structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://portfolio-git-main-hayden-huffmans-projects.vercel.app/",
+  integrations: [preact()]
+});
